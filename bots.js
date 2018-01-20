07b21bb176a7fc871401168a870c2053efaa588b
jcgter777 <34496783+jcgter777@users.noreply.github.com>
2018-01-19 20:47:24 -0500

    Create bots.js

---
+++ bots.js
@@ -0,0 +1,14 @@
+var FriendlyBOT = new Bot("FriendlyBOT", "o");
+function initializeBots() {
+  FriendlyBOT.register();
+}
+FriendlyBOT.executeCommand =  function(data) {
+  var poster = data.poster;
+  var message = data.message;
+  var timestamp = data.timestamp;
+  var raw_timestamp = data.rawTimestamp;
+ 
+   if (message.substring(0,1) == "/") {
+  	FriendlyBOT.respond("/me waves back at " + poster);
+  }
+}
