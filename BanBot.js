fe4f2ba2476635c50c9e81da2138de1e3f7bcf14
jcgter777 :P <34496783+jcgter777@users.noreply.github.com>
2018-01-28 13:38:22 -0500

    help
    @legend-of-iPhoenix CHECK

--- BanBot.js
+++ BanBot.js
@@ -8,6 +8,7 @@ BanBot.executeCommand =  function(data) {
   var timestamp = data.timestamp;
   var raw_timestamp = data.rawTimestamp;
  
+  getArgs("/ban username")[1]
    if (message.substring(0,3) == "ban") {
   	BanBot.respond(user + " has been banned for ~1 eternity.");
   }
